f53058fd0be30661ab44f31083dbeb8c08f4cc65
olamideare <63550820+olamideare@users.noreply.github.com>
2022-07-30 20:26:06 +0100

    Facebook Auth

--- app.js
+++ app.js
@@ -84,6 +84,18 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3300/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
